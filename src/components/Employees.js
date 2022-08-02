7dede081aca0396e8a1536f1f5f639b956a5efb0
Jnewton26 <99002127+Jnewton26@users.noreply.github.com>
2022-08-01 23:42:24 -0500

    Add files via upload

---
+++ src/components/Employees.js
@@ -0,0 +1,18 @@
+const Employees = [
+    {
+        id: "1",   
+        Name: "Kat Wenzler",
+        Age: "46"
+    },{
+        id: "2",   
+        Name: "Susan Taylor",
+        Age: "52"
+    },{
+        id: "3",   
+        Name: "Kathy Kam",
+        Age: "50"
+    } 
+    ]
+   
+    export default Employees;
+    
